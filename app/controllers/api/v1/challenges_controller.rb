97fade3deae66f4ce1dfd48c7c819f388a260db4
yadhu98 <yadhu98@gmail.com>
2025-04-20 12:38:51 +0530

    added update,delete,view one apis

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -19,6 +19,33 @@ module Api
         end
       end
 
+      def show
+        challenge = Challenge.find(params[:id])
+        if challenge
+          render json: challenge
+        else
+          render json: { message: 'Challenge not found' }
+        end
+      end
+
+      def update
+        challenge = Challenge.find(params[:id])
+        if challenge.update(challenge_params)
+          render json: { message: 'updated successfully' }
+        else
+          render json: { message: 'challenge not found' }
+        end
+      end
+
+      def destroy
+        challenge = Challenge.find(params[:id])
+        if challenge.destroy
+          render json: { message: 'Challenge deleted' }
+        else
+          render json: { message: 'Challenge not found' }
+        end
+      end
+
       private
 
       def challenge_params
